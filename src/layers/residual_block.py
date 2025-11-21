1cc269938f7316a98bd324c99acb2f33ed177a9c
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-11-22 01:31:47 +0300

    Update residual_block.py

--- src/layers/residual_block.py
+++ src/layers/residual_block.py
@@ -2,21 +2,23 @@ import torch
 import torch.nn as nn
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, use_identity=True):
+    def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
-        self.use_identity = use_identity and in_channels == out_channels and stride == 1
 
-        # 3x3 conv branch
+        # 3x3 conv branch 
         self.conv3x3 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn3x3 = nn.BatchNorm2d(out_channels)
 
-        # 1x1 conv branch (shortcut if dims mismatch)
-        if not self.use_identity:
+        # 1x1 conv branch
+        self.use_1x1 = in_channels != out_channels or stride != 1
+        if self.use_1x1:
             self.conv1x1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0, bias=False)
             self.bn1x1 = nn.BatchNorm2d(out_channels)
         else:
             self.conv1x1 = None
             self.bn1x1 = None
+
+        self.use_identity = (in_channels == out_channels and stride == 1)
 
         self.relu = nn.ReLU(inplace=True)
 
@@ -25,7 +27,7 @@ class ResidualBlock(nn.Module):
 
         if self.conv1x1 is not None:
             out += self.bn1x1(self.conv1x1(x))
-        elif self.use_identity:
+        if self.use_identity:
             out += x
 
         return self.relu(out)
