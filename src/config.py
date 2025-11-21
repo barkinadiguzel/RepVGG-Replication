1c163cf1b5a2920b419ea058f13b16c7329bcb27
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-11-22 01:34:45 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,14 @@
+REP_VGG_CONFIG = {
+    "A": {
+        "num_blocks": [1, 2, 4, 14, 1],
+        "width_mult": [1.0, 1.0, 1.0, 1.0, 1.0],
+    },
+    "B": {
+        "num_blocks": [1, 4, 6, 16, 1],
+        "width_mult": [1.0, 1.0, 1.0, 1.0, 1.25],
+    }
+}
+
+INPUT_SIZE = 224
+NUM_CLASSES = 1000
+USE_IDENTITY = True
